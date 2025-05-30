6e795d16a285408707a7d7fba6ce38d0789f492d
chanhung <tangchanhungit@gmail.com>
2025-05-30 15:02:35 +0700

    [update] add more case for advance search

---
+++ cypress/support/step_definitions/advanceSearch.cy.js
@@ -0,0 +1,55 @@
+import { SearchPage } from "../../pageObjects/searchpage";
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { checkCardContainsKeyword, handleIfResultsExist } from "../commands";
+
+Given("I am on the search page", () => {
+    cy.visit('/viec-lam-it')
+});
+
+When('I select {string} in the location filter', (location) => {
+  SearchPage.selectLocation(location);
+});
+
+When('I select {string} in the experience filter', (experience) => {
+  SearchPage.selectFilterItem('Experience Level',experience);
+});
+
+When('I select {string} in the benefits filter', (benefit) => {
+  SearchPage.selectFilterItem('Benefits',benefit);
+});
+
+When('I select {string} in the company size filter', (companySize) => {
+  SearchPage.selectFilterItem('Company Size',companySize);
+});
+
+When('I select {string} in the work type filter', (workType) => {
+  SearchPage.selectFilterItem('Work Type', workType);
+});
+
+Then('I press the search button again', () =>{
+  SearchPage.clickSearchButtonInPage();
+});
+
+Then('all results should be in {string}', (location) => {
+  handleIfResultsExist(() => {
+    SearchPage.jobCards
+      .should('exist')
+      .each(($card) => {
+          Cypress._.each($card, (card) => {
+          checkCardContainsKeyword(card, location);  
+        });
+      });
+  });
+});
+
+Then('all results should require {string}', (keyword) => {
+  handleIfResultsExist(() => {
+    SearchPage.jobCards
+      .should('exist')
+      .each(($card) => {
+          Cypress._.each($card, (card) => {
+          checkCardContainsKeyword(card, keyword);  
+        });
+      });
+  });
+});
--- cypress/support/step_definitions/search.cy.js
+++ cypress/support/step_definitions/search.cy.js
@@ -4,11 +4,10 @@ import {
     When,
     Then,
   } from "@badeball/cypress-cucumber-preprocessor";
-import { checkCardContainsKeyword } from "../commands";
+import { checkCardContainsKeyword, handleIfResultsExist} from "../commands";
 
 Given('I am on the TopDev search page', () => {
     SearchPage.navigate();
-    cy.wait(1000);
 });
 
 When('I enter {string} in search bar', (keyword) => {
@@ -61,52 +60,18 @@ Then('the search result should be {string}', (expected) => {
   }
 });
 
-// Then('I should see at least {int} result', (minCount) => {
-//     SearchPage.jobCards.should('have.length.at.least', minCount);
-// });
+Then('I should see at least {int} result', (minCount) => {
+  handleIfResultsExist( ()=>{
+    SearchPage.jobCards.should('have.length.at.least', minCount);
+  })
+});
+
 
 
 Then('Each result should contain {string}', (keyword) => {
-  // Kiểm tra nếu danh sách kết quả tồn tại
-  SearchPage.jobList.then(($list) => {
-    if ($list.length === 0) {
-      cy.log('Không có phần danh sách job nào hiển thị trên trang.');
-      return;
-    }
-
-    // Nếu có danh sách job, tiếp tục kiểm tra các job card
-    SearchPage.jobCards.then(($cards) => {
-      const count = $cards.length;
-
-      if (count === 0) {
-        cy.log('Không có kết quả job nào để kiểm tra.');
-        // Optional: kiểm tra có thông báo "Không tìm thấy kết quả"
-        cy.contains('Không tìm thấy kết quả').should('exist');
-      } else {
-        cy.log(`Tìm thấy ${count} job, kiểm tra từng job có chứa từ khóa "${keyword}"...`);
-        cy.wrap($cards).each(($card) => {
-          cy.wrap($card)
-            .invoke('text')
-            .then((text) => {
-              expect(text.toLowerCase()).to.include(keyword.toLowerCase());
-            });
-        });
-      }
+  SearchPage.jobCards.then(($cards) => {
+    Cypress._.each($cards, (card) => {
+        checkCardContainsKeyword(card, keyword);  
     });
   });
-  
-  // Kiểm tra các free job cards (nếu có)
-  // SearchPage.freeJob.then(($freeJobContainers) => {
-  //   if ($freeJobContainers.length > 0) {
-  //     cy.wrap($freeJobContainers).each(($li) => {
-  //       const $jobs = Cypress.$($li).find('a[class*="w-1/2"]');
-  //       if ($jobs.length > 0) {
-  //         $jobs.each((index, jobEl) => {
-  //           const $job = Cypress.$(jobEl);
-  //           checkCardContainsKeyword($job, keyword);
-  //         });
-  //       }
-  //     });
-  //   }
-  // });
 });
